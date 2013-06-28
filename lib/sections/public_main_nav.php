2e6a2167e54fcf7cfc52b59e9f9577d64b5d1825
appslogix <patrick@appslogix.ca>
2013-06-28 19:12:43 -0400

    Added links "Find Space" and "List Space" to public nav
    This two links will be linked to files list_space.php and
    find_space.php on the root folder of the site.

--- lib/sections/public_main_nav.php
+++ lib/sections/public_main_nav.php
@@ -1,7 +1,9 @@
 	<table align="center" width="100%" cellspacing="1" cellpadding="1" border="0">
 		<tr>
 			<td align="left">
-            	<a href="index.php">Home</a> | 
+            	<a href="index.php">Home</a> |
+                <a href="find_space.php">Find Space</a> |
+                <a href="list_space.php">List Space</a> | 
                 <a href="login.php">Log in</a> | 
                 <a href="register.php">Register</a> | 
                 <a href="pass_reset.php">Reset Password</a> | 
