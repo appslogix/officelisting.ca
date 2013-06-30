c8f717a58d9ba9c7ed60006410b4d6c13dd8d7ef
appslogix <patrick@appslogix.ca>
2013-06-30 00:03:45 -0400

    created user featured listing include for user account index

---
+++ lib/sections/user_featured_listings.php
@@ -0,0 +1,33 @@
+<h2>Featured Offices</h2>
+
+<?php
+
+require_once('../lib/connections/db.php');
+
+?>
+
+<?php
+// Make the query:
+$sql = 'SELECT * FROM spaces INNER JOIN buildings ON spaces.building_id = buildings.id ORDER BY space_id ASC';
+
+$res = mysql_query ($sql);
+while ($row = mysql_fetch_array($res)) {
+
+?>
+<div>
+<p><img src="images/spaces/<?=$row['image']?>" width="150"/><br />
+<?php
+	$address = $row['address'];
+?>
+<span><?=$address?></span><br />
+<span>Suite: <?=$row['suite_number']?></span><br />
+<span>Square Footage: <?=$row['square_footage']?></span><br />
+<span>Monthly Rents: <?=$row['monthly_rents']?></span><br />
+<a href="add_favourite.php?space_id=<?=$row['space_id']?>">Add Favourite</a> | <a href="space_detail.php?id=<?=$row['space_id']?>">View Details</a>
+</p>
+
+</div>
+
+<?php
+}
+?>
