65d5151ce545e89a89f817fc650cde494304da86
appslogix <patrick@appslogix.ca>
2013-06-29 23:56:13 -0400

    Added manage favourite and manage listing to user main nav

--- lib/sections/user_main_nav.php
+++ lib/sections/user_main_nav.php
@@ -1,9 +1,9 @@
 <div align="right">
     	<a href="index.php">Home</a> | 
         <a href="find_space.php">Find Space</a> |
-        <a href="#">Manage My Favourites</a> |
+        <a href="manage_favourites.php">Manage My Favourites</a> |
         <a href="list_space.php">List Space</a> |
-        <a href="#">Manage My Listings</a> |
+        <a href="manage_listings.php">Manage My Listings</a> |
         <? if (!empty($getuser[0]['thumb_path'])){echo "<a href='manage_photo.php'>Manage My Photo</a> | ";} else {echo "<a href='upload_photo.php'>Upload Photo</a> | ";} ?>
         <a href="edit_profile.php">Edit Profile</a> |
         <a href="change_pass.php">Change Password</a> |  
